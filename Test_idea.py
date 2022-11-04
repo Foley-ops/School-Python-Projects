e8ecddbea31c58381fddad9ffecb79298ad76e3f
xXsma11poXx <115027712+xXsma11poXx@users.noreply.github.com>
2022-11-04 11:45:21 -0500

    Overhaul
    Created functions, cleaned inputs, changed for loop to activate if loan would go on forever, changed cvs output to show month 0 as having no interest or payment.

--- Test_idea.py
+++ Test_idea.py
@@ -1,65 +1,93 @@
 import csv
 
+
+def print_stat():
+    if initial_loan == 0:
+        print(f'Initial loan amount: ${loan_token:.2f}')
+        print(f'Loan term is: {years} years and {months} months')
+        print(f'Total amount paid: ${total_monthly_payments:.2f}')
+        print(f'Total interest paid: ${monthly_interest_c:.2f}')
+    elif initial_loan != 0:
+        print(f'Initial loan amount: ${loan_token:.2f}')
+        print(f'Total amount paid: ${total_monthly_payments:.2f}')
+        print(f'Loan amount leftover: ${amount_leftover:.2f}')
+        print(f'Total interest paid: ${monthly_interest_c:.2f}')
+
+
+def writer():
+    loan_writer.writerow(
+        [f'{month_count}', f'{initial_loan:.2f}', f'{monthly_interest_c:.2f}',
+         f'{total_monthly_payments:.2f}'])
+
+
 month_count = 0
 monthly_interest_c = 0
 total_monthly_payments = 0
 
-x = 'test'
-initial_loan = 10_000
-percent_apr = 2.75
-monthly_payment = 1_000
 
-# x = input('Enter desired file name: ')
-# initial_loan = float(input('What is the amount of the loan? '))
-# percent_apr = float(input('What is your APR? '))
-# monthly_payment = float(input('How much will you pay per month? '))
+x = input('Enter desired file name: ')
+initial_loan = loan_token = float(input('What is the amount of the loan? '))
+percent_apr = float(input('What is your APR? '))
+monthly_payment = float(input('How much will you pay per month? '))
 
 actual_apr = percent_apr / 100
-amount_after = initial_loan - monthly_payment
-monthly_interest = (amount_after * actual_apr) / 12
-remaining_amount = amount_after + monthly_interest
+monthly_interest = (loan_token * actual_apr) / 12
 total_monthly_payments += monthly_payment
+total_cost_token = loan_token + (loan_token * actual_apr)
 
-row2 = [month_count, f'{initial_loan:.2f}', f'{monthly_interest:.2f}', f'{total_monthly_payments:.2f}']
-# print(f'Amount paid :{total_monthly_payment:.2f}')
-# print(f'monthly interest :{monthly_interest:.2f}')
-# print(f'remaining amount :{remaining_amount:.2f}')
-
+row2 = [month_count, f'{initial_loan:.2f}',
+        f'{0}', f'{0}']
 loan_file = open(f'{x}.csv', 'w')
-fieldnames = ('Month', 'Loan Amount', 'Interest', 'Amount Paid')
+fieldnames = ('Month', 'Loan Amount', 'Interest Accrued', 'Amount Paid')
 loan_writer = csv.writer(loan_file)
 loan_writer.writerow(fieldnames)
 loan_writer.writerow(row2)
 
 
-test_1 = monthly_interest * 30
-test_2 = monthly_payment * 30
-test_3 = initial_loan - (test_1 + test_2)
-
-loan_amount_c = initial_loan
+loan_and_apr = initial_loan + monthly_interest
+after_payment = loan_and_apr - monthly_payment
 
 
-if 0 < test_3:
+if initial_loan <= after_payment:
+
     for i in range(0, 30):
         month_count += 1
-        initial_loan = (initial_loan - monthly_payment) + monthly_interest
-        total_monthly_payments += monthly_payment
-        monthly_interest_c += monthly_interest
-        loan_writer.writerow(
-            [f'{month_count}', f'{initial_loan:.2f}', f'{monthly_interest_c:.2f}', f'{total_monthly_payments:.2f}'])
 
-elif test_3 <= 0:
-    while 0 <= remaining_amount:
-        month_count += 1
-        remaining_amount -= (monthly_payment + monthly_interest)
-        initial_loan = ((initial_loan - monthly_payment) + monthly_interest)
-        total_monthly_payments += monthly_payment
-        monthly_interest_c += monthly_interest
-        loan_writer.writerow(
-            [f'{month_count}', f'{initial_loan:.2f}', f'{monthly_interest_c:.2f}',
-             f'{total_monthly_payments:.2f}'])
+        if month_count == 1:
+            total_monthly_payments = monthly_payment
+            monthly_interest_c = monthly_interest
+        else:
+            total_monthly_payments += monthly_payment
+            monthly_interest_c += monthly_interest
+
+        initial_loan = (initial_loan - monthly_payment) + monthly_interest
+        amount_leftover = initial_loan
+        writer()
 
 else:
-    print('trash')
+    while 0 < initial_loan:
+        month_count += 1
+
+        if month_count == 1:
+            total_monthly_payments = monthly_payment
+            monthly_interest_c = monthly_interest
+        else:
+            total_monthly_payments += monthly_payment
+            monthly_interest_c += monthly_interest
+
+        initial_loan = (initial_loan - monthly_payment) + monthly_interest
+
+        writer()
+
+        if initial_loan < monthly_payment:
+            month_count += 1
+            monthly_payment = initial_loan
+            total_monthly_payments += monthly_payment
+            initial_loan -= monthly_payment
+            writer()
+years = month_count // 12
+months = month_count % 12
+
+print_stat()
 
 loan_file.close()
